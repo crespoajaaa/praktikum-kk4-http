6f6cd531bc407bee6e8497c4a62ba6e5f16a4b59
Crespo <ajalink389@gmail.com>
2023-11-02 10:19:07 +0700

    first commit

---
+++ anjay.js
@@ -0,0 +1,66 @@
+const http = require('http');
+
+const todos = [
+{ id: 1, text:'Todo one'},
+  { id: 2, text:'Todo two'},
+  { id: 3, text:'Todo Three'},
+ ];
+
+const server = http.createServer((req, res)=>{
+    // listening data from client
+    const {method, url} = req;
+    let body = [];
+
+      req.on('data', chunk => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        let status = 404;
+        const response = {
+          success: false,
+          Result: [],
+          error: ''
+        };
+
+        if (method === 'GET' && url === '/todos') {
+
+          status = 200;
+          response.success = true;
+          response.result = todos;
+      
+        } else if (method === 'POST' && url === '/todos') {
+          const { id, text } = JSON.parse(body);
+      
+          if  (!id || !text) {
+            status = 400;
+            response.error = 'Please add id and text';
+          }else {
+            todos.push({id, text});
+            status = 201;
+            response.success = true;
+            response.result = todos;
+          }
+        }
+      
+      
+        res.writeHead(status, {
+          'content-type': 'application/json',
+          'X-Powered-By': 'Node.js',
+        })
+      
+        res.end(JSON.stringify(response));
+      })
+  
+});
+
+const port = 3006;
+
+// const server = http.createServer((rq, res) => {
+//   console.log("gua kece bgt...")
+//   res.end("punya gue!\n");
+// });
+
+server.listen(port, () => {
+  console.log(`Sudah lari cuyy:${port}`);
+});
